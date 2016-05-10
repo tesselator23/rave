fb7460111fa084026019a8c587f4bbfa734e0173
Te55elator <dobby-z@hotmail.com>
2016-05-11 09:52:45 +1000

    First commit

---
+++ app/src/main/java/au/edu/deakin/rave_app/adapter/GridViewAdapter.java
@@ -0,0 +1,73 @@
+package au.edu.deakin.rave_app.adapter;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import au.edu.deakin.rave_app.R;
+
+import java.util.ArrayList;
+
+public class GridViewAdapter extends BaseAdapter {
+
+    public ArrayList<Drawable> allItemsResourceID;
+    private LayoutInflater inflater;
+    Context context;
+
+    public GridViewAdapter(Context context, ArrayList<Drawable> images) {
+        inflater = LayoutInflater.from(context);
+        this.context = context;
+//
+        allItemsResourceID = images;
+        Log.d("Adapter", "Create Image Adapter " + allItemsResourceID.size());
+    }
+    GridViewAdapter(Context context) {
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return allItemsResourceID.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return allItemsResourceID.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        final ViewHolder holder;
+        View view = convertView;
+        if (view == null) {
+            view = inflater.inflate(R.layout.image_inflater, parent, false);
+            holder = new ViewHolder();
+            assert view != null;
+            holder.imageView = (ImageView) view.findViewById(R.id.ivImageInflator);
+            view.setTag(holder);
+        } else {
+            holder = (ViewHolder) view.getTag();
+        }
+
+        if (!allItemsResourceID.get(position).equals("")) {
+            holder.imageView.setImageDrawable(allItemsResourceID.get(position));
+        }
+
+        return view;
+    }
+
+}
+
+class ViewHolder {
+    ImageView imageView;
+}
---
+++ app/src/main/java/au/edu/deakin/rave_app/fragments/FragmentTwo.java
@@ -0,0 +1,53 @@
+package au.edu.deakin.rave_app.fragments;
+
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import au.edu.deakin.rave_app.R;
+import au.edu.deakin.rave_app.activity.MainActivity;
+import au.edu.deakin.rave_app.adapter.ListViewAdapter;
+
+import java.util.ArrayList;
+
+
+/**
+ * Created by Mehul on 03-Dec-2015.
+ */
+public class FragmentTwo extends Fragment
+{
+    private ListView listView;
+    TypedArray allContacts;
+    ArrayList<String> allContactNames = new ArrayList<>();
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_two, null);
+        getAllWidgets(rootView);
+        setAdapter();
+        return rootView;
+    }
+
+    private void getAllWidgets(View view) {
+        listView = (ListView) view.findViewById(R.id.listFragmentTwo);
+
+        allContacts = getResources().obtainTypedArray(R.array.all_contacts);
+
+    }
+
+    private void setAdapter()
+    {
+        for (int i = 0; i < allContacts.length(); i++) {
+            allContactNames.add(allContacts.getString(i));
+        }
+
+        ListViewAdapter listViewAdapter= new ListViewAdapter(MainActivity.getInstance(), allContactNames);
+        listView.setAdapter(listViewAdapter);
+    }
+}
