040e04f4cd8114f1938dc1ca266faf9b5f5a6626
Te55elator <dobby-z@hotmail.com>
2016-05-16 13:32:39 +1000

    Uber Launch
    Working

--- app/src/main/java/au/edu/deakin/rave_app/fragments/FragmentThree.java
+++ app/src/main/java/au/edu/deakin/rave_app/fragments/FragmentThree.java
@@ -1,11 +1,16 @@
 package au.edu.deakin.rave_app.fragments;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import com.google.android.gms.maps.GoogleMap;
 
 import com.google.android.gms.maps.MapView;
@@ -25,9 +30,7 @@ public class FragmentThree extends Fragment implements OnMapReadyCallback {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    public void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState);   }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -37,6 +40,27 @@ public class FragmentThree extends Fragment implements OnMapReadyCallback {
             mMapView = (MapView) rootView.findViewById(R.id.map);
             mMapView.onCreate(savedInstanceState);
             mMapView.getMapAsync(this);
+            View view = inflater.inflate(R.layout.fragment_three, container, false);
+            Button uber = (Button) rootView.findViewById(R.id.uber);
+            uber.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    try {
+                        PackageManager pm = getActivity().getPackageManager();
+                        pm.getPackageInfo("com.ubercab", PackageManager.GET_ACTIVITIES);
+                        String uri =
+                                "uber://?action=setPickup&pickup=my_location&client_id=YOUR_CLIENT_ID";
+                        Intent intent = new Intent(Intent.ACTION_VIEW);
+                        intent.setData(Uri.parse(uri));
+                        startActivity(intent);
+                    } catch (PackageManager.NameNotFoundException e) {
+                        // No Uber app! Open mobile website.
+                        String url = "https://m.uber.com/sign-up?client_id=YOUR_CLIENT_ID";
+                        Intent i = new Intent(Intent.ACTION_VIEW);
+                        i.setData(Uri.parse(url));
+                        startActivity(i);
+                    }
+                }
+            });
         } catch (InflateException e) {
 
         }
